18ad0f9fb1c9f075a8a649c051ba5ae431f20751
Houssem Lachtar <lachtarhoussem4@gmail.com>
2022-06-14 14:18:50 +0100

    Update harp.js

--- js/harp.js
+++ js/harp.js
@@ -1,12 +1,9 @@
 /**
  * harp.js
- * http://www.codrops.com
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2016, Codrops
- * http://www.codrops.com
+ *
  */
 ;(function(window) {
 
